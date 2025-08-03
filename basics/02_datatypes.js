5608da45d6e52bedb7a9832d1fc9e479fbc30c9c
Jay-2810 <128506529+Jay-2810@users.noreply.github.com>
2025-08-03 08:20:18 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // Treat all js code as newer version
+// alert(3+3) // Not working beacuse we use node not browser
+
+let name="Jay"
+let age=18
+let isTrue=false
+let add=null
+let city
+// number=> 2^53
+// bigint
+// string=""
+// null=> standalone value
+// undefined=> undefined don't have eny type
+// symbol=> unique
+// object=> null is a object
+
+// console.table(typeof[name,age,isTrue,add,city]); =>don't work
+console.table([name,age,isTrue])
+console.log(typeof add) // null is object
+console.log(typeof city)
