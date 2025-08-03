d4c10cfc03f2c43008c45bd51beda13382e809d3
Jay-2810 <128506529+Jay-2810@users.noreply.github.com>
2025-08-03 09:12:33 +0000

    Conversion

--- basics/03_conversionOps.js
+++ basics/03_conversionOps.js
@@ -24,3 +24,34 @@ let somenumber=1000
 let stringsomenumber=String(somenumber);
 console.log(typeof(stringsomenumber));
 console.log(stringsomenumber);
+
+//******OPERATIONS******//
+let value=18;
+let negvalue=-value;
+// console.log(negvalue);
+
+// console.log(value+11);
+// console.log(18+11);
+// console.log(18/11);
+// console.log(18%11);
+// console.log(18-11);
+// ** use for power
+console.log(2**3);
+
+// conversion
+// if in converion their is any string present than it convert to string
+// but if before string their is 2 numbers which can be add than first they add and than convert to string.
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(true);
+// below two console turn into the number because of '+'
+console.log(+true);
+console.log(+false);
+
+// Last ae have postfix and prefix ops.
+let age=18;
+console.log(age++);
+console.log(++age);
