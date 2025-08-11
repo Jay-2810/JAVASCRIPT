9a8f002c5802b9f5026b6ecfe855140d344873bb
Jay-2810 <128506529+Jay-2810@users.noreply.github.com>
2025-08-11 06:12:03 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,3 +35,42 @@ function loginuser_1(username){
 }
 console.log(loginuser_1());
 // if we pass any name than it will override JAY
+
+// only give first value
+function calculateCartPrice(num1){
+    return num1;
+}
+console.log(calculateCartPrice(200,300));
+// ... is the rest operator which give all the value like below
+function calculateCartPrice_1(...num1){
+    return num1;
+}
+console.log(calculateCartPrice_1(200,300,400));
+
+// First value go in val1 likewise second go in val2 and rest of go num1
+function calculateCartPrice_2(val1,val2,...num1){
+    return num1;
+}
+console.log(calculateCartPrice_2(200,300,400,500,600));
+
+const user={
+    username: "JAY",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleObject(user);
+// we can direct pass the object in a function
+handleObject({
+    username: "Smit",
+    price: 399
+});
+
+const myArr=[1,2,3,4,5];
+function returnSecondValue(Arr)
+{
+    return Arr[1];
+}
+console.log(returnSecondValue(myArr));
